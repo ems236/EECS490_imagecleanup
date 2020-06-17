25b4b280e4d1fc8653c63ab9062b934ca9395571
Ellis <ems236@case.edu>
2020-06-17 08:05:33 -0400

    Fixed watermarks

--- waterMarkOrigin.m
+++ waterMarkOrigin.m
@@ -4,8 +4,8 @@ function [positionX, positionY]=waterMarkOrigin(img, watermark)
     centerY = ceil(height / 2);
     
     [waterWidth, waterHeight, ~] = size(watermark);
-    offsetWaterX = ceil(waterWidth / 2) - 1;
-    offsetWaterY = ceil(waterHeight / 2) - 1;
+    offsetWaterX = ceil(waterWidth / 2);
+    offsetWaterY = ceil(waterHeight / 2);
     
     offsetWaterX = offsetWaterX - (mod(width, 2) * mod(waterWidth, 2));
     offsetWaterY = offsetWaterY - (mod(height, 2) * mod(waterHeight, 2));
